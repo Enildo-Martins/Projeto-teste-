33c65ec5e7800572e2a8f255c122067b8e290005
Enildo-Martins <enildooliveira1@gmail.com>
2025-02-26 19:56:27 -0300

    Versão 3.0 - Adicionada saudação baseada na hora da maquina

--- Versionamento.java
+++ Versionamento.java
@@ -1,16 +1,30 @@
-// - versao 2.0
+// versao 3.0
 
+import java.time.LocalTime;
 import java.util.Scanner;
 
 public class Versionamento {
+
+    public static String saudacao() {
+        int hora = LocalTime.now().getHour();
+
+        if (hora < 12) {
+            return "Bom dia";
+        } else if (hora < 18) {
+            return "Boa tarde";
+        } else {
+            return "Boa noite";
+        }
+    }
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Ola primeira versão");
-        String nome;
-        System.out.println("Digite seu nome:" );
-        nome = sc.nextLine();
+        System.out.print("Digite seu nome: ");
+        String nome = scanner.nextLine();
 
-        System.out.printf("Olá %s",nome);
+        System.out.println(saudacao() + ", " + nome + "!");
+
+        scanner.close();
     }
 }

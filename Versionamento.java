a9f3956294263402cc1b2a7f5e3ad9b8d84cc7ef
Enildo-Martins <enildooliveira1@gmail.com>
2025-02-26 19:51:35 -0300

    Versão 2.0 - Adicionada interação com o usuario

--- Versionamento.java
+++ Versionamento.java
@@ -1,7 +1,16 @@
-// - versao 1.0
+// - versao 2.0
+
+import java.util.Scanner;
 
 public class Versionamento {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Ola primeira versão");
+        String nome;
+        System.out.println("Digite seu nome:" );
+        nome = sc.nextLine();
+
+        System.out.printf("Olá %s",nome);
     }
 }
